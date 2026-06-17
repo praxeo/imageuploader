311ce4a0571a17d7a0ca6f8a74a0d75ff1457de8
praxeo <181277231+praxeo@users.noreply.github.com>
2026-06-17 08:18:06 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "photo-relay",
+  "type": "module",
+  "scripts": {
+    "dev": "wrangler dev",
+    "deploy": "wrangler deploy"
+  },
+  "dependencies": {
+    "qrcode": "^1.5.4"
+  },
+  "devDependencies": {
+    "wrangler": "latest"
+  }
+}
